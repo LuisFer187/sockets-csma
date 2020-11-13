8f5cdcd32e865b59e2674214bb1c82385663b67e
navy <navy@NavyKali.star.com>
2020-11-13 00:15:33 -0800

    clase pA añadida, y los metodos sendResponse y cpnstructor de pA con sobrecarga, y los getters y setters para sus variables

--- src/puntodeacceso/pA.java
+++ src/puntodeacceso/pA.java
@@ -10,13 +10,13 @@ package puntodeacceso;
  * @author navy
  */
 public class pA {
-    String strMiIP = "";
-    String strMensajeroIP = "";
-    int intPaquetes = 0;
-    String strEstado = "";
-    String strEstadoAnterior = "";
-    String strEstadoMensajero = "";
-    String strEstadoMensajeroAnterior = "";
+    private String strMiIP = "";
+    private String strMensajeroIP = "";
+    private int intPaquetes = 0;
+    private String strEstado = "";
+    private String strEstadoAnterior = "";
+    private String strEstadoMensajero = "";
+    private String strEstadoMensajeroAnterior = "";
     public void pA(){
     
     }
@@ -28,31 +28,138 @@ public class pA {
             String strEstadoAnterior,
             String strEstadoMensajero,
             String strEstadoMensajeroAnterior){
-        this.strEstado = strEstado;
-        this.strMiIP = strMiIP;
-        this.strMensajeroIP = strMensajeroIP;
-        this.strEstadoAnterior = strEstadoAnterior;
-        this.strEstadoMensajero = strEstadoMensajero;
-        this.strEstadoMensajeroAnterior = strEstadoMensajeroAnterior;
-        this.intPaquetes = intPaquetes;
+        this.setStrEstado(strEstado);
+        this.setStrMiIP(strMiIP);
+        this.setStrMensajeroIP(strMensajeroIP);
+        this.setStrEstadoAnterior(strEstadoAnterior);
+        this.setStrEstadoMensajero(strEstadoMensajero);
+        this.setStrEstadoMensajeroAnterior(strEstadoMensajeroAnterior);
+        this.setIntPaquetes(intPaquetes);
     }
     // El punto puede estar en los estados LIBRE, (OCUPADO+IPMENSAJERO)
     public void puntoAcceso(){
-        if (this.strEstado=="LIBRE") {
-            this.strEstadoAnterior="LIBRE";
-            this.strEstado="OCUPADO"+this.strMensajeroIP;
-            sendResponse("CTS",this.strMensajeroIP);
+        if (this.getStrEstado()=="LIBRE") {
+            this.setStrEstadoAnterior("LIBRE");
+            this.setStrEstado("OCUPADO" + this.getStrMensajeroIP());
+            sendResponse("CTS", this.getStrMensajeroIP());
         }
-        else if (this.strEstado=="OCUPADO"+this.strMensajeroIP) {
-            sendResponse("Recibiendo datos de "+this.strMensajeroIP,this.strMensajeroIP);
+        else if (this.getStrEstado()=="OCUPADO"+this.getStrMensajeroIP()) {
+            sendResponse("Recibiendo datos de "+this.getStrMensajeroIP(), this.getStrMensajeroIP());
             
         }
-        else if (this.strEstado.contains("OCUPADO")==true){
-            sendResponse("OCUPADO",this.strMensajeroIP);
+        else if (this.getStrEstado().contains("OCUPADO")==true){
+            sendResponse("OCUPADO", this.getStrMensajeroIP());
         }
     }
     private void sendResponse(String estado, String strMensIP) {
         System.out.println("ESTADO: "+estado);
         System.out.println("ENVIAR A: "+strMensIP);
     }
+    
+    
+    
+    
+    
+    
+    
+    // Getters Y Setters
+
+    /**
+     * @return the strMiIP
+     */
+    public String getStrMiIP() {
+        return strMiIP;
+    }
+
+    /**
+     * @param strMiIP the strMiIP to set
+     */
+    public void setStrMiIP(String strMiIP) {
+        this.strMiIP = strMiIP;
+    }
+
+    /**
+     * @return the strMensajeroIP
+     */
+    public String getStrMensajeroIP() {
+        return strMensajeroIP;
+    }
+
+    /**
+     * @param strMensajeroIP the strMensajeroIP to set
+     */
+    public void setStrMensajeroIP(String strMensajeroIP) {
+        this.strMensajeroIP = strMensajeroIP;
+    }
+
+    /**
+     * @return the intPaquetes
+     */
+    public int getIntPaquetes() {
+        return intPaquetes;
+    }
+
+    /**
+     * @param intPaquetes the intPaquetes to set
+     */
+    public void setIntPaquetes(int intPaquetes) {
+        this.intPaquetes = intPaquetes;
+    }
+
+    /**
+     * @return the strEstado
+     */
+    public String getStrEstado() {
+        return strEstado;
+    }
+
+    /**
+     * @param strEstado the strEstado to set
+     */
+    public void setStrEstado(String strEstado) {
+        this.strEstado = strEstado;
+    }
+
+    /**
+     * @return the strEstadoAnterior
+     */
+    public String getStrEstadoAnterior() {
+        return strEstadoAnterior;
+    }
+
+    /**
+     * @param strEstadoAnterior the strEstadoAnterior to set
+     */
+    public void setStrEstadoAnterior(String strEstadoAnterior) {
+        this.strEstadoAnterior = strEstadoAnterior;
+    }
+
+    /**
+     * @return the strEstadoMensajero
+     */
+    public String getStrEstadoMensajero() {
+        return strEstadoMensajero;
+    }
+
+    /**
+     * @param strEstadoMensajero the strEstadoMensajero to set
+     */
+    public void setStrEstadoMensajero(String strEstadoMensajero) {
+        this.strEstadoMensajero = strEstadoMensajero;
+    }
+
+    /**
+     * @return the strEstadoMensajeroAnterior
+     */
+    public String getStrEstadoMensajeroAnterior() {
+        return strEstadoMensajeroAnterior;
+    }
+
+    /**
+     * @param strEstadoMensajeroAnterior the strEstadoMensajeroAnterior to set
+     */
+    public void setStrEstadoMensajeroAnterior(String strEstadoMensajeroAnterior) {
+        this.strEstadoMensajeroAnterior = strEstadoMensajeroAnterior;
+    }
+    
 }
